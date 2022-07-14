d39e9d0b71a07fd9316f6b03aff1144628f71f85
Daphne <daphneruth304@gmail.com>
2022-07-14 09:09:09 +0300

    usestate change in component to trigger re-evaluation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
+import Button from './components/UI/Button/Button';
 
 import './App.css';
 
 function App() {
   const[showPara,setShowPara] = useState(false)
+
+  const toggleParaHandler = () => {
+    //changing state
+    setShowPara(prevShowPara => ! prevShowPara)
+  }
   return (
     <div className="app">
       <h1>Hi there!</h1>
      { showPara && <p>react behind scenes</p>}
+     <Button onClick ={toggleParaHandler}>Toggle Paragrap</Button>
     </div>
   );
 }
