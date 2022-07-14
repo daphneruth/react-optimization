69dd2f8c25850b37ab02f4bf16496370c4ac954d
Daphne <daphneruth304@gmail.com>
2022-07-14 10:21:25 +0300

    using useCallback hook to prevent re-creation of the function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Button from './components/UI/Button/Button';
 import DemoOutput from './components/Demo/DemoOutput';
 
@@ -8,10 +8,10 @@ function App() {
   console.log('App Running')
   const[showPara,setShowPara] = useState(false)
 
-  const toggleParaHandler = () => {
+  const toggleParaHandler = useCallback(() => {
     //changing state
     setShowPara(prevShowPara => ! prevShowPara)
-  }
+  }, []);
   return (
     <div className="app">
       <h1>Hi there!</h1>
